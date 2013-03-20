a5192a8220fbd7e4d8f4dc692cf194cc864e2dce
Dan Bentley <danbentley9@gmail.com>
2013-03-20 14:54:21 +0000

    Fixing unit tests

--- test/parse-commands-test.js
+++ test/parse-commands-test.js
@@ -1,21 +1,23 @@
 describe("ParseCommands tests", function() {
 
+    var grid = new Grid();
+
     it("Parse basic commands (non-repeatable)", function() {
         var commandString = 'left\n right\n up\n down';
-        var commands = parseCommandString(commandString);
+        var commands = grid.parseCommandString(commandString);
         expect(commands.length).toEqual(4);
     });
 
     it("Parse repeatable commands", function() {
         var commandString = 'left\n right\n move(10)';
-        var commands = parseCommandString(commandString);
+        var commands = grid.parseCommandString(commandString);
         expect(commands.length).toEqual(12);
         expect(commands.indexOf('move(10)')).toBe(-1);
     });
 
     it("Parse repeatable commands in middle of list", function() {
         var commandString = 'left\n right\n move(10)\n up\n down';
-        var commands = parseCommandString(commandString);
+        var commands = grid.parseCommandString(commandString);
         expect(commands.length).toEqual(14);
         expect(commands.indexOf('move(10)')).toBe(-1);
     });
