e08ae8d2ae8877e1909f3481fcf7e3639ac7a8fa
Dan Bentley <danbentley9@gmail.com>
2013-03-20 13:08:41 +0000

    Grid is now an object

--- js/common.js
+++ js/common.js
@@ -20,107 +20,105 @@ function parseCommandString(commandString) {
 	return commands;
 }
 
-$(document).ready(function() {
-
-	var gridCount = 1;
-	var validCommands = ['up', 'down', 'left', 'right', 'move'];
-
-	var grid = {
-		dimensions: {
-			x: 16,
-			y: 16,
-		}
+function Grid(options) {
+	this.gridCount = 1;
+	this.validCommands = ['up', 'down', 'left', 'right', 'move'];
+	this.$gridEl;
+	this.$commands = $('#commands');
+	this.robot;
+	this.dimensions = {
+		x: 16,
+		y: 16,
 	};
+	this.start();
+}
 
-	var gridEl;
-	var $commands = $('#commands');
+Grid.prototype.start = function() {
+	this.addListeners();
+	this.draw();
+	$gridEl = $('div#grid');
+	this.robot = new Robot($gridEl);
+	this.refresh();
+};
 
-	function start() {
-		addListeners();
-		draw();
-		gridEl = $('div#grid');
-		robot = new Robot(gridEl);
-		refresh();
-	}
+Grid.prototype.addListeners = function() {
 
-	function addListeners() {
+	$('a.up').on('click', function(e) {
+		$commands.val($commands.val() + 'up\n');
+		e.preventDefault();
+	});
 
-		$('a.up').on('click', function(e) {
-			$commands.val($commands.val() + 'up\n');
-			e.preventDefault();
-		});
+	$('a.down').on('click', function(e) {
+		$commands.val($commands.val() + 'down\n');
+		e.preventDefault();
+	});
 
-		$('a.down').on('click', function(e) {
-			$commands.val($commands.val() + 'down\n');
-			e.preventDefault();
-		});
+	$('a.left').on('click', function(e) {
+		$commands.val($commands.val() + 'left\n');
+		e.preventDefault();
+	});
 
-		$('a.left').on('click', function(e) {
-			$commands.val($commands.val() + 'left\n');
-			e.preventDefault();
-		});
+	$('a.right').on('click', function(e) {
+		$commands.val($commands.val() + 'right\n');
+		e.preventDefault();
+	});
 
-		$('a.right').on('click', function(e) {
-			$commands.val($commands.val() + 'right\n');
-			e.preventDefault();
-		});
+	$('form#code').on('submit', function(e) {
 
-		$('form#code').on('submit', function(e) {
+		var commands = parseCommandString($commands.val())
+		executeCommands(commands);
 
-			var commands = parseCommandString($commands.val())
-			executeCommands(commands);
+		e.preventDefault();
+	});
+};
 
-			e.preventDefault();
-		});
-	}
+Grid.prototype.executeCommands = function(commands) {
 
+	var intervalId;
 
-	function executeCommands(commands) {
+	clearInterval(intervalId);
+	intervalId = setInterval(function() {
+		var command = commands.shift();
+		if (commands.length === 0) {
+			clearInterval(intervalId);
+			return;
+		}
+		if (validCommands.indexOf(command) > -1) {
+			robot[command]();
+		}
+	}, 500);
+};
 
-		var intervalId;
+Grid.prototype.draw = function() {
+	this.drawGrid();
+	this.drawMaze();
+};
 
-		clearInterval(intervalId);
-		intervalId = setInterval(function() {
-			var command = commands.shift();
-			if (commands.length === 0) {
-				clearInterval(intervalId);
-				return;
-			}
-			if (validCommands.indexOf(command) > -1) {
-				robot[command]();
-			}
-		}, 500);
-	}
+Grid.prototype.refresh = function() {
+	$(window).trigger('grid.refresh');
+};
 
-	function draw() {
-		drawGrid();
-		drawMaze();
-	}
-
-	function refresh() {
-		$(window).trigger('grid.refresh');
-	}
-
-	function drawGrid(i) {
-		var markup = '<div id="grid">';
-		for (var x = 0; x < grid.dimensions.x; x++) {
-			for (var y = 0; y < grid.dimensions.y; y++) {
-				markup += '<span id="grid-' + x + '-' + y + '" />';
-			};
+Grid.prototype.drawGrid = function(i) {
+	var markup = '<div id="grid">';
+	for (var x = 0; x < this.dimensions.x; x++) {
+		for (var y = 0; y < this.dimensions.y; y++) {
+			markup += '<span id="grid-' + x + '-' + y + '" />';
 		};
-		markup += '</div>';
-		$('div.grids').append(markup);
-	}
+	};
+	markup += '</div>';
+	$('div.grids').append(markup);
+};
 
-	function drawMaze() {
+Grid.prototype.drawMaze = function() {
 
-		var wallClasses = ['wall-up', 'wall-down', 'wall-left', 'wall-right'];
+	var wallClasses = ['wall-up', 'wall-down', 'wall-left', 'wall-right'];
 
-		$('span[id^=grid-]').each(function() {
-			wallClass = wallClasses[Math.floor((Math.random() * 4) + 1)];
-			$(this).addClass(wallClass);
-		});
-	}
+	$('span[id^=grid-]').each(function() {
+		wallClass = wallClasses[Math.floor((Math.random() * 4) + 1)];
+		$(this).addClass(wallClass);
+	});
+};
 
-	start();
+$(document).ready(function() {
+	var grid = new Grid();
 });
